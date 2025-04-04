e53ce1b7bb813d228211d747dc17fa9eebcf4622
Nahuel González <nahuelgonzalez775@gmail.com>
2025-04-04 19:05:37 -0300

    Modificacion de header

--- Front-Ecommerce/front-ecommerce/src/components/header/Header.jsx
+++ Front-Ecommerce/front-ecommerce/src/components/header/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div>header</div>
+    <div>
+      <h1>Perfumes</h1>
+      <h2>estoy es un ecommerce </h2>
+      
+    </div>
   )
 }
 
