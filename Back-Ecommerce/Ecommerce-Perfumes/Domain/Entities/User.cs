ba53d7c91f39df55151f23f3f60450a834714ada
Cristian Ninotti <cristianninotti03@gmail.com>
2025-04-14 15:54:55 -0300

    Creacion de usuario abstracto y Minorista

---
+++ Back-Ecommerce/Ecommerce-Perfumes/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public bool Available { get; set; } = true;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string NameAccount { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Dni { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string TypeUser { get; set; } = string.Empty;
+    }
+}
