f5a7efd9cc12af9bc4d76d97808ab549caf1ef57
Geraldine Martinez <geralmartinez000@gmail.com>
2025-04-25 21:29:27 -0300

    Prueba de un componente con tailwind, instale dep

--- Front-Ecommerce/front-ecommerce/src/App.jsx
+++ Front-Ecommerce/front-ecommerce/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="w-full min-h-screen flex flex-col">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
