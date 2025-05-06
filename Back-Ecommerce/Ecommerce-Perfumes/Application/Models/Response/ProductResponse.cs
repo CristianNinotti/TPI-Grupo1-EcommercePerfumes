cd61fec21be858e8ec89e60e35b3740091d7b1b2
Cristian Ninotti <cristianninotti03@gmail.com>
2025-05-06 20:03:18 -0300

    Marca y Genero agregado como atributos a Product

--- Back-Ecommerce/Ecommerce-Perfumes/Application/Models/Response/ProductResponse.cs
+++ Back-Ecommerce/Ecommerce-Perfumes/Application/Models/Response/ProductResponse.cs
@@ -7,6 +7,8 @@
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string Marca { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
         public int CategoryId { get; set; }
         public List<string>? Photos { get; set; }
         public bool Available { get; set; }
---
+++ Back-Ecommerce/Ecommerce-Perfumes/Infrastructure/Migrations/20250506225148_Producto con marca y genero.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Productoconmarcaygenero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genero",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Marca",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genero",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Marca",
+                table: "Products");
+        }
+    }
+}
