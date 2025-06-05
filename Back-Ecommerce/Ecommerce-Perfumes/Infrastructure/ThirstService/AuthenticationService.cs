303f08b3515b6c72bebefc23996cc34b71c5aa91
Juan Ignacio Zucco <juani_z29@hotmail.com>
2025-06-04 22:09:54 -0300

    Ventanas modales en sección de recuperación de contraseña
    Co-authored-by: Francisco Poli Veliz <Francisco480@users.noreply.github.com>
    Co-authored-by: Nahuel González <Nahue430@users.noreply.github.com>
    Co-authored-by: Facundo Solari <facundosolari@users.noreply.github.com>

--- Back-Ecommerce/Ecommerce-Perfumes/Infrastructure/ThirstService/AuthenticationService.cs
+++ Back-Ecommerce/Ecommerce-Perfumes/Infrastructure/ThirstService/AuthenticationService.cs
@@ -123,7 +123,7 @@ public class AuthenticationService : IAuthenticationService
             user = _superAdminRepository.GetAllSuperAdmins().FirstOrDefault(u => u.Email == email);
 
         if (user == null)
-            throw new Exception("No se encontró un usuario con ese email.");
+            throw new Exception("No se encontró un usuario con ese email :D.");
 
         var token = Guid.NewGuid().ToString();
         user.PasswordResetToken = token;
